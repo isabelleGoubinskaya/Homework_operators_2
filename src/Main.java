b1c70ef91a6fc02578b80c0b1f71c46f1d998035
Isabelle <isagoubinskaya@gmail.com>
2022-11-23 23:08:35 +0100

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        byte clientOS = 0;
+        if (clientOS == 0){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("bad entry");
+        }
+    }
+}

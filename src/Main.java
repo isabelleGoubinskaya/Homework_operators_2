3536375e5234590e134b1e6a5c1a06d9dd315618
Isabelle <isagoubinskaya@gmail.com>
2022-11-23 23:43:29 +0100

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
         byte clientOS = 0;
         short clientDeviceYear = 2015;
-
         if (clientOS == 0){
             if (clientDeviceYear < 2015){
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
@@ -19,7 +17,14 @@ public class Main {
         } else {
             System.out.println("bad entry");
         }
+
+        int year = 2022;
+
+        if (year % 100 == year % 400 && year % 4 == 0) {
+            System.out.println( "год " + year + "  является високосным");
+        } else {
+            System.out.println( "год " + year + " не является високосным");
+        }
+
     }
-
-
 }

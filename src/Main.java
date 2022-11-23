87618f6d5d4ac463006655a514f8cb799b900cd2
Isabelle <isagoubinskaya@gmail.com>
2022-11-24 00:34:54 +0100

    Задание 5

--- src/Main.java
+++ src/Main.java
@@ -34,5 +34,48 @@ public class Main {
             System.out.println("Потребуется дней: " + delivery);
         }
 
+        short monthNumber = 12;
+        switch (monthNumber){
+            case 1:
+                System.out.println("winter");
+                break;
+            case 2:
+                System.out.println("winter");
+                break;
+            case 3:
+                System.out.println("spring");
+                break;
+            case 4:
+                System.out.println("spring");
+                break;
+            case 5:
+                System.out.println("spring");
+                break;
+            case 6:
+                System.out.println("summer");
+                break;
+            case 7:
+                System.out.println("summer");
+                break;
+            case 8:
+                System.out.println("summer");
+                break;
+            case 9:
+                System.out.println("autumn");
+                break;
+            case 10:
+                System.out.println("autumn");
+                break;
+            case 11:
+                System.out.println("autumn");
+                break;
+            case 12:
+                System.out.println("winter");
+                break;
+
+                default:
+                System.out.println("Incorrect month number");
+                break;
+        }
     }
 }
